9df0b2967387d5d5a6937e70502bfb488a9e7b0c
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-07-18 12:04:57 -0400

    updated Package.swift for Swift 6 with swift-embedded-foundation dependency

--- Package.swift
+++ Package.swift
@@ -1,6 +1,34 @@
+// swift-tools-version: 6.1
 import PackageDescription
 
 let package = Package(
     name: "JSON",
-    exclude: ["Xcode", "Carthage"]
+    products: [
+        .library(
+            name: "JSON",
+            targets: ["JSON"]
+        )
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/PureSwift/swift-embedded-foundation.git",
+            branch: "master"
+        )
+    ],
+    targets: [
+        .target(
+            name: "JSON",
+            dependencies: [
+                .product(
+                    name: "FoundationEmbedded",
+                    package: "swift-embedded-foundation"
+                )
+            ]
+        ),
+        .testTarget(
+            name: "JSONTests",
+            dependencies: ["JSON"]
+        )
+    ],
+    swiftLanguageModes: [.v6]
 )
